a93702851d59cdf0fb5e7d1853399c8275e76089
Thomas Laporte <thomas.laporte03@gmail.com>
2016-06-29 23:06:01 +0200

    Angry bird

--- Assets/Scripts/DragAndDrop.cs
+++ Assets/Scripts/DragAndDrop.cs
@@ -4,37 +4,47 @@ using System.Collections.Generic;
 
 public class DragAndDrop : MonoBehaviour {
 	private List<GameObject> lstBirds = new List<GameObject> ();
+	private int birdID;
+	private Vector3 posBird = new Vector3(0,0);
 //
 //
 	//private int idBird = GameObject.Find ("Main Camera").GetComponent<MainScript>().getIdBird;
 
 	// Use this for initialization
 	void Start () {
-		lstBirds = GameObject.Find ("Main Camera").GetComponent<MainScript>().lstBirds;
+//		lstBirds = GameObject.Find ("Main Camera").GetComponent<MainScript>().lstBirds;
+//		birdID = GameObject.Find ("Main Camera").GetComponent<MainScript> ().getIdBird;
 	}
 	
 	// Update is called once per frame
 	void Update () {
+		lstBirds = GameObject.Find ("Main Camera").GetComponent<MainScript>().lstBirds;
+		birdID = GameObject.Find ("Main Camera").GetComponent<MainScript> ().getIdBird;
 	}
 
 	void OnMouseDown()
 	{
-
+		Debug.Log ("TEST");
 	}
 	
 	void OnMouseDrag() {
+		
+
 		Vector3 posMouse = Camera.main.ScreenToWorldPoint(Input.mousePosition);
 		Vector3 realMousePosition = new Vector3 (posMouse.x, posMouse.y, 0f);
 		Vector3 posCatapulte = new Vector3(GameObject.Find ("Main Camera").GetComponent<MainScript> ().catapult.transform.position.x,GameObject.Find ("Main Camera").GetComponent<MainScript> ().catapult.transform.position.y, 0f);
 
+	
 		//lstBirds [0].transform.position = Input.mousePosition;
 		Vector3 testt = posCatapulte;
 		Vector2 test = new Vector2 (Camera.main.ScreenToWorldPoint (Input.mousePosition).x, Camera.main.ScreenToWorldPoint (Input.mousePosition).y);
-		if (Mathf.Round(Vector3.Distance (realMousePosition, posCatapulte)) < 12.5f) {
+		if (Mathf.Round(Vector3.Distance (realMousePosition, posCatapulte)) < 2f) {
+			posBird = realMousePosition;
 			testt = realMousePosition;
-			lstBirds [0].transform.position = test;
+			lstBirds [birdID].transform.position = test;
+			lstBirds [birdID].transform.rotation = new Quaternion(0,0,0,0);
 		} else
-			lstBirds [0].transform.position = posCatapulte;
+			lstBirds [birdID].transform.position = posBird;
 	}
 	
 	void OnMouseUp()
--- Assets/Scripts/MainScript.cs
+++ Assets/Scripts/MainScript.cs
@@ -3,145 +3,137 @@ using UnityEngine;
 using UnityEngine.UI;
 
 public class MainScript : MonoBehaviour {
-    
 
-    // public GameObject bird;
-
-    public List<GameObject> lstBirds = new List<GameObject>();
-    public Text txtMessage, txtScore;
-//	public GameObject camera;
+	public List<GameObject> lstBirds = new List<GameObject>();
+	public Text txtMessage, txtScore;
+	//	public GameObject camera;
 	public GameObject catapult;
 
-    public bool clicActive = true;
-    private int idBird = 0;
+	public bool clicActive = true;
+	private int idBird = 0;
 
-    private int score = 0;
+	private int score = 0;
 
-    // Test  time 
-    private GameObject currentBird;
-    private float startTime = 0f;
-    private float elapsedTime;
+	// Test  time 
+	private GameObject currentBird;
+	private float startTime = 0f;
+	private float elapsedTime;
+	private Vector3 initialCamPos; // Position initiale de la caméra 
+	private Vector3 initialBirdPos;
 
 	public bool followBird = false;
-    // Update is called once per frame
-    void Update () {
-		
 
+	void Start()
+	{
+		initialCamPos = GetComponent<Camera>().transform.position;
+		initialBirdPos = lstBirds [0].transform.position;
+	}
 
-        txtScore.text = "Score : " + score; 
+	void Update () {
 
-        // Propulser l'oiseau lors du clic sur le bouton gauche
-//		if (Input.GetMouseButtonUp(0) && clicActive && idBird<= lstBirds.Count)
-//        {
-//            clicActive = false;
-//
-//            startTime = Time.time;
-//
-//			followBird = true; // La camera suit l'oiseau lance
-//
-//            addForceOnBirds();
-//            
-//        }
-       
-        // Si l'oiseau n'a pas touché le sol, il est automatiquement détruit au bout de 3 secondes
-        elapsedTime = Time.time - startTime;
-		Vector3 posBird = lstBirds [idBird].transform.position;
-		if (startTime != 0f && currentBird != null && System.Math.Round (elapsedTime) == 3) {
+		txtScore.text = "Score : " + score; 
+
+		// Si l'oiseau n'a pas touché le sol, il est automatiquement détruit au bout de 3 secondes
+		elapsedTime = Time.time - startTime;
+		//Vector3 posBird = lstBirds [idBird].transform.position;
+		Vector3 posBird = initialBirdPos;
+
+		if(idBird <= lstBirds.Count -1) 
+		{posBird= lstBirds [idBird].transform.position;}
+		if (startTime != 0f && currentBird != null && System.Math.Round (elapsedTime) == 5) {
 			if (lstBirds [idBird].transform.position == posBird) {
 				destroyBird ();
 				followBird = false;
 			}
 		} 
 		else {
-			Vector3 temp = GetComponent<Camera>().transform.position; // copy to an auxiliary variable...
+			Vector3 posCamFollowBird = GetComponent<Camera>().transform.position; 
 			if(followBird)
 			{
-				// TEst de modification de la position de la camera 
-
-				temp.x = lstBirds[idBird -1].transform.position.x; // modify the component you want in the variable...
-				GetComponent<Camera>().transform.position = temp; // and save the modified value 
+				// La camera suit la position de l'oiseau qui est jeté
+				posCamFollowBird.x = lstBirds[idBird -1].transform.position.x; 
+				GetComponent<Camera>().transform.position = posCamFollowBird; 
 			}
-//			else
-//			{
-//				temp.x = Camera.main.ScreenToWorldPoint(Input.mousePosition).x;
-//				GetComponent<Camera>().transform.position = temp;
-//			}
+			else
+			{
+				GetComponent<Camera>().transform.position = initialCamPos;
+			}
+			//			else
+			//			{
+			//				temp.x = Camera.main.ScreenToWorldPoint(Input.mousePosition).x;
+			//				GetComponent<Camera>().transform.position = temp;
+			//			}
 		}
-			
-
-    }
-
-    public void addForceOnBirds()
-    {
-
-        //Debug.Log(Input.mousePosition.x - GameObject.Find("Slingshot").transform.position.x);
-        currentBird = lstBirds[idBird];
-        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-
-        // récupération du rigidbody de la pomme afin de lui appliquer une force dans une direction aléatoire
-        Rigidbody2D rb = lstBirds[idBird].GetComponent<Rigidbody2D>();
-
-        // Force appliquée à l'oiseau pour l'envoyer 
 
 
-		Vector3 posMouse = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+	}
+
+	public void addForceOnBirds()
+	{
+		startTime = Time.time;
+		//Debug.Log(Input.mousePosition.x - GameObject.Find("Slingshot").transform.position.x);
+		currentBird = lstBirds[idBird];
+		Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+		// Récupération du rigidbody de l'oiseau courant pour lui appliquer une force
+		Rigidbody2D rb = lstBirds[idBird].GetComponent<Rigidbody2D>();
+
+		Vector3 posMouse = Camera.main.ScreenToWorldPoint(Input.mousePosition); 
 		Vector3 realMousePosition = new Vector3 (posMouse.x, posMouse.y, 0f);
 
-		//Debug.DrawLine(realMousePosition, catapult.transform.position, Color.red, 40000000000000);
+//		Debug.DrawLine(realMousePosition, catapult.transform.position, Color.red, 40000000000000);
 
-		Vector3 lance = catapult.transform.position - realMousePosition;
-	
-		rb.velocity  = lance * 8f;
-		Debug.Log (Vector3.Distance(realMousePosition, catapult.transform.position));
-//		rb.AddForce(lance * 50f, ForceMode2D.Force); // new Vector2(Input.mousePosition.x , Input.mousePosition.y) * 2, ForceMode2D.Force
+		Vector3 lance = catapult.transform.position - lstBirds [idBird].transform.position;//realMousePosition;
 
-        // Incrémentation pour le prochain oiseau
-        idBird++;
-    }
+		rb.velocity  = lance * 12f;
+		//		rb.AddForce(lance * 50f, ForceMode2D.Force); // new Vector2(Input.mousePosition.x , Input.mousePosition.y) * 2, ForceMode2D.Force
 
-    /// <summary>
-    /// Récupère l'oiseau à instancier
-    /// </summary>
-    public int getIdBird
-    {
-        get {return idBird;}
-    }
+		// Incrémentation pour le prochain oiseau
+		idBird++;
+	}
 
-    /// <summary>
-    /// Destruction de l'oiseau lancé
-    /// </summary>
-    public void destroyBird()
-    {
-        txtScore.text = 
-        txtMessage.text = "+ 100pts";
-        txtMessage.transform.position = currentBird.transform.position;
+	/// <summary>
+	/// Récupère l'oiseau à instancier
+	/// </summary>
+	public int getIdBird
+	{
+		get {return idBird;}
+	}
 
-        score += 100;
+	/// <summary>
+	/// Destruction de l'oiseau lancé
+	/// </summary>
+	public void destroyBird()
+	{
+		txtMessage.transform.position = currentBird.transform.position;
+		txtMessage.text = "+ 100pts";
 
-        Destroy(currentBird);
+		score += 100; // Incrementation du score
 
-        if (idBird <= lstBirds.Count - 1)
-        {
-            Debug.Log("L'id du bird  = " + idBird);
-            lstBirds[idBird].transform.position = new Vector2(-6.5f, -3f); // Positionnement de l'oiseau dans le lance-pierre
-            Rigidbody2D rb = lstBirds[idBird].GetComponent<Rigidbody2D>();
+		Destroy(currentBird);
 
-            rb.Sleep(); // Permet d'éviter à l'oiseau de tomber une fois qu'on a changé sa position
-            clicActive = true;
-        }
-    }
+		if (idBird <= lstBirds.Count - 1)
+		{
+			lstBirds[idBird].transform.position = initialBirdPos; //new Vector2(-6.5f, -3f); // Positionnement de l'oiseau dans le lance-pierre
+			Rigidbody2D rb = lstBirds[idBird].GetComponent<Rigidbody2D>();
 
-    public int Score
-    {
-        get
-        {
-            return score;
-        }
-        set
-        {
-            score = value;
-        }
-    }
+			rb.Sleep(); // Permet d'éviter à l'oiseau de tomber une fois qu'on a changé sa position
+			clicActive = true;
+		}
+		startTime = 0f;
+	}
+
+	public int Score
+	{
+		get
+		{
+			return score;
+		}
+		set
+		{
+			score = value;
+		}
+	}
 
 
 
